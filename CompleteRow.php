ca0559e46c2fe43e5e47df1d0c41c5ce4109bb14
Hauke Bartsch <HaukeBartsch@gmail.com>
2018-08-20 10:31:16 -0500

    first version

--- CompleteRow.php
+++ CompleteRow.php
@@ -14,9 +14,42 @@ class CompleteRow extends AbstractExternalModule {
 		   $color = $this->getColor();
                         ?>
 <script type='text/javascript'>
-    (function(window, document, $) {
+
+// color all rows that have values assigned by the user
+  function updateBackgrounds(color) {
+     jQuery('tbody tr').each(function() {
+       // for each row we check now if there is at least one entry in there that has been set
+       existingEntries = false;
+       // every field that requires values has an input (assumption)
+       var inputs = jQuery(this).find('input');
+       // for each of the inputs in this row see if at least one entry has a value (muliple-choice answers have multiple inputs)
+       for (var i = 0; i < inputs.length; i++) {
+         if (inputs[i].type == "radio" && inputs[i].checked) {
+            //console.log("Found set in " + jQuery(this).attr('id') + " as " + inputs[i].value + " input: " + inputs[i]);
+	    existingEntries = true; // at least one input in this tr is not empty
+	    break;	    
+	 } else if ( inputs[i].type == "text" && inputs[i].value !== "") {
+            //console.log("Found set in " + jQuery(this).attr('id') + " as " + inputs[i].value + " input: " + inputs[i]);
+	    existingEntries = true; // at least one input in this tr is not empty	 
+	    break;
+         }
+       }
+       if (existingEntries) {
+          //jQuery(this).find('td').css('background-color', '#dbf7df');
+          jQuery(this).find('td').css('background-color', color);
+       } else if (inputs.length > 0) {
+          // console.log("Nothing set in " + jQuery(this).attr('id'));
+          // jQuery(this).find('td').css('background-color', '#999');
+       }
+     });
+  }
+
+     (function(window, document, $) {
         $(document).ready(function() {
-	    console.log("HI in ready: use color" + <?php echo($color); ?>);
+	    color = "<?php echo($color); ?>";
+ 	    jQuery('tbody tr').click(function() {
+   	       updateBackgrounds(color);
+            });
         });
      })(window, document, jQuery);
 </script>
@@ -29,11 +62,46 @@ class CompleteRow extends AbstractExternalModule {
 		   $color = $this->getColor();
                         ?>
 <script type='text/javascript'>
+  // color all rows that have values assigned by the user
+  function updateBackgrounds() {
+     jQuery('tbody tr').each(function() {
+       // for each row we check now if there is at least one entry in there that has been set
+       existingEntries = false;
+       // every field that requires values has an input (assumption)
+       var inputs = jQuery(this).find('input');
+       // for each of the inputs in this row see if at least one entry has a value (muliple-choice answers have multiple inputs)
+       for (var i = 0; i < inputs.length; i++) {
+         if (inputs[i].type == "radio" && inputs[i].checked) {
+            //console.log("Found set in " + jQuery(this).attr('id') + " as " + inputs[i].value + " input: " + inputs[i]);
+	    existingEntries = true; // at least one input in this tr is not empty
+	    break;	    
+	 } else if ( inputs[i].type == "text" && inputs[i].value !== "") {
+             //console.log("Found set in " + jQuery(this).attr('id') + " as " + inputs[i].value + " input: " + inputs[i]);
+	     // we could have a calculated field, in that case we don't want to mark it
+	     if (jQuery(inputs[i]).attr('readonly') == "readonly") {
+		 break;
+	     }
+	     existingEntries = true; // at least one input in this tr is not empty	 
+	     break;
+         }
+       }
+       if (existingEntries) {
+          jQuery(this).find('td').css('background-color', '#dbf7df');
+       } else if (inputs.length > 0) {
+          // console.log("Nothing set in " + jQuery(this).attr('id'));
+          // jQuery(this).find('td').css('background-color', '#999');
+       }
+     });
+  }
+
     (function(window, document, $) {
         $(document).ready(function() {
-	    console.log("HI in ready: use color" + <?php echo($color); ?>);
-
-        });
+	    color = "<?php echo($color); ?>";
+     	    jQuery('tbody tr').click(function() {
+               //console.log("Someone clicked on something (in redcap_data_entry_form)");
+	       updateBackgrounds(color);
+            });
+     });
      })(window, document, jQuery);
 </script>
                         <?php
